b13b7d19f7e749103b13dcb156159721e266fe35
Eric Brosch <ebrosch@gmail.com>
2026-09-24 17:17:18 -0400

    fix: updating target files

--- simple-flask-app/app.py
+++ simple-flask-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Docker Compose!\n'
+    return 'Hello from inside a Docker Container!\n'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
